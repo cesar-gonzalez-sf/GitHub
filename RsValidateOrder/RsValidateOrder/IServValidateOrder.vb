398844f0e3b303e44c23a349b2542a8d20efc9c0
cesar-gonzalez-sf <git@github.com:cesar-gonzalez-sf/GitHub.git>
2020-06-05 18:57:18 -0400

    NEW

---
+++ RsValidateOrder/RsValidateOrder/IServValidateOrder.vb
@@ -0,0 +1,12 @@
+﻿<ServiceContract()>
+Public Interface IServValidateOrder
+
+    <OperationContract()>
+    <WebInvoke(UriTemplate:="ServicePOR",
+               Method:="POST",
+               RequestFormat:=WebMessageFormat.Json,
+               ResponseFormat:=WebMessageFormat.Json,
+               BodyStyle:=WebMessageBodyStyle.WrappedRequest)>
+    Function ServicePOR(ByVal RequestPOR As PropertysPaymentInfoRequest) As PropertysPaymentInfoResponse
+
+End Interface
---
+++ RsValidateOrder/RsValidateOrder/ServValidateOrder.svc.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Runtime.Serialization
+Imports System.Runtime.Serialization.Json
+Imports Newtonsoft.Json.Linq
+Imports Newtonsoft.Json
+Imports RestSharp
+Imports System.Net
+
+
+Public Class ServValidateOrder
+    Implements IServValidateOrder
+
+    Private Sub AgregaHeaderPorCORS()
+        'Se agrega headers por CORS
+        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", My.Settings.AccessControlAllowOrigin)
+        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "POST")
+        HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type, Accept")
+        HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", My.Settings.AccessControlMaxAge)
+    End Sub
+
+    Function ServicePOR(ByVal RequestPOR As PropertysPaymentInfoRequest) As PropertysPaymentInfoResponse Implements IServValidateOrder.ServicePOR
+
+        AgregaHeaderPorCORS()
+
+        Dim Resp As New PropertysPaymentInfoResponse
+
+        Dim Proceso As New PaymentProcess
+
+        Dim ClsBd As New ClsProcesos
+
+        Dim Result As Boolean
+
+        Result = Proceso.PGetPaymentInfoPOR(RequestPOR, Resp)
+
+        Dim ListSku As String
+        ListSku = ""
+        For Each item As PaymentItems In Resp.items
+            ListSku = ListSku & ";" & item.sku & "," & item.monto
+        Next
+
+        Dim Nro_interno As String = ""
+
+        Nro_interno = ClsBd.mpGetInternalNumber(RequestPOR.storeId, "124655757", ListSku, RequestPOR.ordenVenta)
+
+        If Result Then
+            Return Resp
+        Else
+            Resp.codResponse = 0
+            Resp.descResponse = "No hubo respuesta"
+
+            Return Resp
+        End If
+
+    End Function
+End Class
