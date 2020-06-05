6ac8b042fcf7c412994bbb3ba490a38b24dd4b29
cesar-gonzalez-sf <git@github.com:cesar-gonzalez-sf/GitHub.git>
2020-06-04 23:22:23 -0400

    Levanto el servicio

--- Ws-Test_POR/Ws-Test_POR/ws-test-por.asmx.vb
+++ Ws-Test_POR/Ws-Test_POR/ws-test-por.asmx.vb
@@ -1,18 +1,29 @@
 ﻿Imports System.Web.Services
 Imports System.Web.Services.Protocols
 Imports System.ComponentModel
+Imports System.ServiceModel.Web
 
 ' Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente.
-' <System.Web.Script.Services.ScriptService()> _
+<System.Web.Script.Services.ScriptService()>
 <System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
 <System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
 <ToolboxItem(False)> _
 Public Class ws_test_por
     Inherits System.Web.Services.WebService
 
-    <WebMethod()> _
+    <WebMethod()>
     Public Function HelloWorld() As String
         Return "Hola a todos"
+    End Function
+
+    <WebMethod()>
+    <WebInvoke(UriTemplate:="ServicioPOR",
+                Method:="POST",
+                RequestFormat:=WebMessageFormat.Json,
+                ResponseFormat:=WebMessageFormat.Json,
+                BodyStyle:=WebMessageBodyStyle.WrappedRequest)>
+    Public Function ServicioPOR(ByVal RequestPOR As PropertysPaymentInfoRequest) As PropertysPaymentInfoResponse
+
     End Function
 
 End Class
